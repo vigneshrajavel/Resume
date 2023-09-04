d18943f0e02bf8870d7fa210ddb53245df2bd8fd
vigneshrajavel <86596700+vigneshrajavel@users.noreply.github.com>
2023-09-04 10:38:24 +0530

    update

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,17 +1,14 @@
 import { Container } from "./styles";
-import githubIcon from "../../assets/github-icon.svg"
-import externalLinkIcon from "../../assets/external-link-icon.svg"
+import githubIcon from "../../assets/github-icon.svg";
+import externalLinkIcon from "../../assets/external-link-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
-
-export function Portfolio(){
-  return(
+export function Portfolio() {
+  return (
     <Container id="portfolio">
       <h2>My Portfolio</h2>
 
       <div className="projects">
-
-
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
@@ -44,17 +41,33 @@ export function Portfolio(){
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/vigneshrajavel/fake-news-detection." target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/vigneshrajavel/fake-news-detection."
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
             </header>
             <div className="body">
               <h3>Fack News Detector</h3>
-            {/*   <p>A landing page of a fictitious company proposed by the <a href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-              </p> */}
+              <p><a href="http://127.0.0.1:5000"> Login </a></p>
             </div>
             <footer>
               <ul className="tech-list">
@@ -65,8 +78,6 @@ export function Portfolio(){
             </footer>
           </div>
         </ScrollAnimation>
-
-
       </div>
     </Container>
   );
