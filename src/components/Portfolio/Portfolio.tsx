05aa85612429af10f3ce9868f715a6745de4362e
vigneshrajavel <86596700+vigneshrajavel@users.noreply.github.com>
2023-09-11 14:13:37 +0530

    project

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -67,7 +67,7 @@ export function Portfolio() {
             </header>
             <div className="body">
               <h3>Fack News Detector</h3>
-              <p><a href="http://127.0.0.1:5000"> Login </a></p>
+              <p><a href="https://fake-news-detection-vr.vercel.app/"> Login </a></p>
             </div>
             <footer>
               <ul className="tech-list">
@@ -78,6 +78,49 @@ export function Portfolio() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/vigneshrajavel/CRUD-Nodejs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>CRUD Using Node js </h3>
+              <p><a href="http://localhost:4200"> Click Here </a></p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Node js</li>
+                <li>Express</li>
+                <li>Sequelize</li>
+                <li>Postgres</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+        
       </div>
     </Container>
   );
